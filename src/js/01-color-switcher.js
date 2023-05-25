08b8903e0469971fc67b3bc4d2193589566bffd2
Perian11 <samusevpasha11@gmail.com>
2023-05-25 22:47:24 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let interval = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
+
+function startColorChange() {
+    startButton.disabled = true;
+    interval = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+startButton.addEventListener('click', startColorChange);
+
+
+function stopColorChange() {
+    startButton.disabled = false;
+    clearInterval(interval);
+}
+stopButton.addEventListener('click', stopColorChange);
